3546bcead4654af85196248f7c6f34e85aca335a
Chanakarn Kingkaew <chanakarnkingkaew@wl207013.jaist.ac.jp>
2023-02-17 08:20:34 +0900

    Level 1 - Email and Password

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 
-mongoose.connect("mongodb+srv://chanakarn:12345678Oak@cluster0.ueqgbuk.mongodb.net/UserDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://chanakarn:<Password>@cluster0.ueqgbuk.mongodb.net/UserDB?retryWrites=true&w=majority");
 
 const userSchema ={
     email: String,
